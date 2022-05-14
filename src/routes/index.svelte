b2f528427c6b57a602d7b962f189727450f4fa26
phi <phi1618jp@gmail.com>
2022-05-14 16:56:43 +0900

    setup profile

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let items = [
+    {
+      key: 'name',
+      value: 'phi',
+    },
+    {
+      key: 'twitter',
+      value: '@phi_jp',
+      link: 'https://twitter.com',
+    },
+    {
+      key: 'github',
+      value: 'phi-jp',
+      link: 'https://github.com/phi-jp',
+    },
+    {
+      key: 'zenn',
+      value: 'phi',
+      link: 'https://zenn.dev/phi',
+    },
+  ];
+</script>
+
+<div class='container-960 my64'>
+
+  <h1 class='mb8'>SvelteKit Labs</h1>
+  <p class='mb16'>SvelteKit の実験のための環境です</p>
+
+  <img class='s64' src='/icon.png' />
+  
+  <dl>
+    {#each items as item}
+      <dt>{item.key}</dt>
+      <dd class='ml16'>
+        <a class='text-blue' href='{item.link || ""}' target='_blank'>{item.value}</a>
+      </dd>
+    {/each}
+  </dl>
+
+</div>
